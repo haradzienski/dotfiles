c6e1ffca5f93f92c961cbf494d7ad59f304b44e5
Mikalai Ziuz <mikalai.ziuz@kroger.com>
2020-02-25 11:00:00 -0800

    working version of hammerspoon config

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,52 +1,7 @@
 -- Source:https://github.com/tekezo/Karabiner/issues/814#issuecomment-415388742
 
 -- HANDLE SCROLLING WITH MOUSE BUTTON PRESSED
-local backMouseButton = 3
-local forwardMouseButton = 4
-local deferred = false
-
-function setOverrides(e)
-    overrideOtherMouseDown:stop()
-    overrideOtherMouseUp:stop()
-    hs.eventtap.otherClick(e:location(), 0, pressedMouseButton)
-    overrideOtherMouseDown:start()
-    overrideOtherMouseUp:start()	
-end
-
-overrideOtherMouseDown = hs.eventtap.new({ hs.eventtap.event.types.otherMouseDown }, function(e)
-    -- print("down")    
-    local pressedMouseButton = e:getProperty(hs.eventtap.event.properties['mouseEventButtonNumber'])
-    if backMouseButton == pressedMouseButton or forwardMouseButton == pressedMouseButton 
-    then
-            deferred = true
-            return true
-        end
-end)
-
-overrideOtherMouseUp = hs.eventtap.new({ hs.eventtap.event.types.otherMouseUp }, function(e)
-     -- print("up")
-    local pressedMouseButton = e:getProperty(hs.eventtap.event.properties['mouseEventButtonNumber'])
-    if backMouseButton == pressedMouseButton 
-        then 
-            if (deferred) then
-                setOverrides(e)
-                hs.eventtap.keyStroke({"cmd"}, "[")
-                return true
-            end
-            return false
-        end
-        
-        if forwardMouseButton == pressedMouseButton 
-            then 
-                if (deferred) then
-                    setOverrides(e)
-                    hs.eventtap.keyStroke({"cmd"}, "]")
-                    return true
-                end
-                return false
-            end
-            return false
-end)
+local rightAdditionalButton = 4
 
 local oldmousepos = {}
 local scrollmult = -4	-- negative multiplier makes mouse work like traditional scrollwheel
@@ -54,10 +9,9 @@ local scrollmult = -4	-- negative multiplier makes mouse work like traditional s
 dragOtherToScroll = hs.eventtap.new({ hs.eventtap.event.types.otherMouseDragged }, function(e)
     local pressedMouseButton = e:getProperty(hs.eventtap.event.properties['mouseEventButtonNumber'])
     -- print ("pressed mouse " .. pressedMouseButton)
-    if backMouseButton == pressedMouseButton or forwardMouseButton == pressedMouseButton 
+    if rightAdditionalButton == pressedMouseButton
         then 
             -- print("scroll");
-            deferred = false
             oldmousepos = hs.mouse.getAbsolutePosition()    
             local dx = e:getProperty(hs.eventtap.event.properties['mouseEventDeltaX'])
             local dy = e:getProperty(hs.eventtap.event.properties['mouseEventDeltaY'])
@@ -70,6 +24,4 @@ dragOtherToScroll = hs.eventtap.new({ hs.eventtap.event.types.otherMouseDragged 
         end 
 end)
 
-overrideOtherMouseDown:start()
-overrideOtherMouseUp:start()
 dragOtherToScroll:start()
