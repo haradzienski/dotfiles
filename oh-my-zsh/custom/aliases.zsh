9600f023709b80b274d6f94b17eaa8a69948d118
Mikalai Ziuz <mikalai.ziuz@walmart.com>
2021-04-12 11:23:45 -0700

    tweak git aliases

--- oh-my-zsh/custom/aliases.zsh
+++ oh-my-zsh/custom/aliases.zsh
@@ -1,10 +1,11 @@
 # git
 alias ga='git add -A'
 alias gb='git branch'
-alias gco='git checkout'
 alias gc='git commit'
+alias gco='git checkout'
 alias gd='git diff'
 alias gds='git diff --staged'
-alias gp='git pull'
+alias gf='git fetch --prune'
+alias gp='git pull --prune'
 alias gpu='git push'
 alias gs='git status'
