447b50a06cebd97b9fd01fe5b4f37c7b21447e6a
Mikalai Ziuz <mikalai.ziuz@walmart.com>
2021-04-26 09:58:00 -0700

    add npm and kubectl aliases

--- oh-my-zsh/custom/aliases.zsh
+++ oh-my-zsh/custom/aliases.zsh
@@ -9,3 +9,11 @@ alias gf='git fetch --prune'
 alias gp='git pull --prune'
 alias gpu='git push'
 alias gs='git status'
+
+# kubectl
+alias kcx='kubectl config use-context'
+
+# npm
+alias nix='npm i --save-exact'
+alias nixd='npm i --save-exact --save-dev'
+alias nixp='npm i --save-exact --save'
