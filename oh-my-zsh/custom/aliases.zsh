4491787befd4177374f841f833dc979fe9482fde
Mikalai Ziuz <mikalai.ziuz@walmart.com>
2021-05-21 12:23:59 -0700

    move git aliases back to shell config

--- oh-my-zsh/custom/aliases.zsh
+++ oh-my-zsh/custom/aliases.zsh
@@ -1,3 +1,19 @@
+# git
+alias g='git'
+alias ga='git add'
+alias gaa='git add .'
+alias gb='git branch'
+alias gc='git commit'
+alias gco='git checkout'
+alias gd='git diff'
+alias gds='git diff --staged'
+alias gf='git fetch --prune'
+alias gl="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+alias gp='git pull --prune'
+alias gpu='git push'
+alias gr='git rebase'
+alias gs='git status'
+
 # kubectl
 alias kcx='kubectl config use-context'
 
